d32b3590392ca04ffac405ffb286f33c3357cbfe
Velsimir <nintendocoin@yandex.ru>
2025-06-27 00:47:44 +0300

    Pigs added in game

---
+++ Assets/Game/Scripts/GameLogic/PigLogic/IPig.cs
@@ -0,0 +1,11 @@
+using Game.Scripts.SpawnerLogic;
+using UnityEngine;
+
+namespace Game.Scripts.GameLogic.PigLogic
+{
+    public interface IPig : ISpawnable
+    {
+        public Rigidbody2D RigidBody { get; }
+        public Collider2D Collider2D { get; }
+    }
+}
---
+++ Assets/Game/Scripts/GameLogic/PigLogic/Pig.cs
@@ -0,0 +1,56 @@
+using System;
+using Game.Scripts.SpawnerLogic;
+using UnityEngine;
+
+namespace Game.Scripts.GameLogic.PigLogic
+{
+    [RequireComponent(typeof(CapsuleCollider2D), 
+        typeof(Rigidbody2D))]
+    public class Pig : MonoBehaviour, IPig
+    {
+        [SerializeField] private float _maxHealth = 3;
+        [SerializeField] private float _damageTrashHold = 0.2f;
+        
+        private float _currentHealth;
+        
+        public event Action<ISpawnable> Disappeared;
+        
+        public MonoBehaviour MonoBehaviour => this;
+        public Rigidbody2D RigidBody { get; private set; }
+        public Collider2D Collider2D { get; private set;}
+
+        private void Awake()
+        {
+            RigidBody = GetComponent<Rigidbody2D>();
+            Collider2D = GetComponent<Collider2D>();
+            _currentHealth = _maxHealth;
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            float impactVelocity = collision.relativeVelocity.magnitude;
+
+            if (impactVelocity > _damageTrashHold)
+            {
+                TakeDamage(impactVelocity);
+            }
+        }
+
+        private void TakeDamage(float amount)
+        {
+            if (amount < 0)
+                return;
+            
+            _currentHealth -= amount;
+
+            if (_currentHealth <= 0)
+                Disappear();
+        }
+
+        public void Disappear()
+        {
+            gameObject.SetActive(false);
+            Disappeared?.Invoke(this);
+        }
+    }
+}
